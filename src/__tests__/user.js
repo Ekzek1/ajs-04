e54f6927ef2bafe1062ea0946a405dc67aea7127
mishaDrigval <drigval.ma.11@gmail.com>
2022-08-02 19:28:40 +0300

    0.2

--- src/__tests__/user.js
+++ src/__tests__/user.js
@@ -3,10 +3,10 @@ import { httpGet } from '../http';
     jest.mock('../http');
 
 beforeEach(() => {
-    jest.resetAllMocks();
+  jest.resetAllMocks();
 });
 test('should call loadUser once', () => {
-    httpGet.mockReturnValue(JSON.stringify({}));
-    loadUser(1);
-    expect(httpGet).toBeCalledWith('http://server:8080/users/1');
+  httpGet.mockReturnValue(JSON.stringify({}));
+  loadUser(1);
+  expect(httpGet).toBeCalledWith('http://server:8080/users/1');
 });
